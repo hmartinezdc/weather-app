0746a3247b6307ca8ba97353684740ccab11e049
Hember Martinez <andrehp.9308@gmail.com>
2023-03-08 18:17:45 -0500

    move svg

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,32 +1,32 @@
 const icons = (icon) => {
   switch (icon) {
     case 'Thunderstorm':
-      icon = '/public/icons/thunderstorms-rain.svg';
+      icon = '/public/thunderstorms-rain.svg';
       break;
     case 'Drizzle':
-      icon = '/public/icons/drizzle.svg';
+      icon = '/public/drizzle.svg';
       break;
     case 'Rain':
-      icon = '/public/icons/rain.svg';
+      icon = '/public/rain.svg';
       break;
     case 'Snow':
-      icon = '/public/icons/snow.svg';
+      icon = '/public/snow.svg';
       break;
     case 'snow':
-      icon = '/public/icons/snow.svg';
+      icon = '/public/snow.svg';
       break;
     case 'Mist':
-      icon = '/public/icons/mist.svg';
+      icon = '/public/mist.svg';
       break;
     case 'Clear':
-      icon = '/public/icons/clear-day.svg';
+      icon = '/public/clear-day.svg';
       break;
     case 'Clouds':
-      icon = '/public/icons/clouds.svg';
+      icon = '/public/clouds.svg';
       break;
 
     default:
-      icon = '/public/icons/mist.svg';
+      icon = '/public/mist.svg';
       break;
   }
   return icon;
